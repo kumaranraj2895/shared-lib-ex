71bcbc1eb19e9d3c6bc4c621cfde8b05c9c5de29
kumaranraj2895 <kumaranraj100@gmail.com>
2021-02-12 12:47:50 +0530

    added override-ex

--- vars/override.groovy
+++ vars/override.groovy
@@ -1,18 +1,15 @@
 def call(body = null) {
-    pipeline {
+    node {
         agent any
-        stages {
+       
 		stage ('Clone') {
 	        	checkout([$class: 'GitSCM', branches: [[name: '*/main']], doGenerateSubmoduleConfigurations: false, extensions: [], 				submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/kumaranraj2895/maven-demo-jib']]])
 	        }
             	stage('Build') {
-                	steps {
                     		script {
                         		body != null ? body() : BuildStep()
-                    		}
-                	}
+                    	}
             	}
-        }
     }
 }
 
